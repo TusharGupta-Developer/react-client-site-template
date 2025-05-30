ed55a540bfc34896ad151489b0209670606ea90d
TusharGupta-Developer <tushar.developer127@gmail.com>
2025-05-30 11:34:14 +0530

    Created CircularGallery Component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Services from './pages/Services';
 import Testimonials from './pages/Testimonials';
 import Contact from "./pages/Contact";
 import Footer from "./components/layout/Footer";
+import CircularGallery from './components/pages/CircularGallery';
 
 function App() {
   return (
@@ -18,6 +19,15 @@ function App() {
         <About />
         <Services />
         <Testimonials />
+        <section className="section" id="contact" style={{ backgroundColor: "var(--color-bg-section)" }}>
+          <h2 className="section__title" style={{ marginBottom: '-.5rem' }}>Gallery</h2>
+
+          <div className="container grid">
+            {/* <div style={{ height: '22rem', position: 'relative' }}> */}
+            <CircularGallery bend={0} textColor="#ffffff" borderRadius={0.05} />
+            {/* </div> */}
+          </div>
+        </section>
         <Contact />
       </main>
       <Footer />
