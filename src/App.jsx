d7560e0fa83fe74f6f9deff76ef3a78bbdd6bcf5
TusharGupta-Developer <tushar.developer127@gmail.com>
2025-06-02 19:41:54 +0530

    Created Loader component and implemented it in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Therapist Website Starter Template (React + CSS3)
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/layout/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,8 +10,28 @@ import Contact from "./pages/Contact";
 import Footer from "./components/layout/Footer";
 import CircularGallery from './components/pages/CircularGallery';
 import ScrollToTop from './components/common/ScrollToTop';
+import Loader from './components/common/Loader';
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading or wait for assets
+    const handleLoad = () => {
+      setTimeout(() => setLoading(false), 700); // Customize delay
+    };
+
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
+  if (loading) return <Loader />;
   return (
     <>
       <Header />
